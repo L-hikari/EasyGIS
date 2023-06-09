40fe613ee70bfc961d3b239f94f685f5f9398eac
L <wangtianliangyaya@gmail.com>
2023-06-09 18:00:01 +0800

    add logo and github link

--- app/Header.jsx
+++ app/Header.jsx
@@ -1,10 +1,25 @@
-
+import Image from "next/image";
+import logo from "./images/easygis.png";
+import github from "./images/github.svg";
+import Link from "next/link";
 
 export default function Header() {
     return (
         <div className="flex justify-between items-center w-full h-10 bg-gray-700">
-            {/* <div className="w-20 text-center text-white">logo</div>
-            <div className="w-20 text-center text-white">github</div> */}
+            <Link href="/" className="w-20 text-center text-white">
+                <Image
+                    src={logo}
+                    alt="easygis-logo"
+                    width={35}
+                />
+            </Link>
+            <a href="https://github.com/L-hikari" target="_blank" className="w-10 text-center text-white">
+                <Image
+                    src={github}
+                    alt="github"
+                    width={35}
+                />
+            </a>
         </div>
     );
 }
